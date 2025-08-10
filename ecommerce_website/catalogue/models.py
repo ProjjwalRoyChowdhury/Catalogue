90a5a04fe5cbfd8740f22e1274789be26fa29bf8
Projjwal Roy Chowdhury <projjwalroychowdhury@gmail.com>
2025-08-10 21:15:13 +0530

    Database Connection

--- ecommerce_website/catalogue/models.py
+++ ecommerce_website/catalogue/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(blank = True)
+
+    class meta():
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(unique = True, max_length= 100)
+    slug = models.SlugField(max_length = 200, unique = True)
+    description = models.TextField()
+    image = models.ImageField(upload_to = "products/")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
--- ecommerce_website/ecommerce_website/settings.py
+++ ecommerce_website/ecommerce_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,10 +38,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "catalog",
+    "catalogue",
     "crispy_forms",
     "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -78,7 +82,7 @@ WSGI_APPLICATION = "ecommerce_website.wsgi.application"
 DATABASES = {
    "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "catalog_db",
+        "NAME": "catalogue_db",
         "USER": "postgres",
         "PASSWORD": "projjwal",
         "HOST": "localhost",
@@ -121,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
